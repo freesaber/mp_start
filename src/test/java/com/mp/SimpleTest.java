8963744a0723cde08d1949dff7b10af3330b0c68
freesaber <free.saber@qq.com>
2022-08-15 17:17:51 +0800

    1

--- src/test/java/com/mp/SimpleTest.java
+++ src/test/java/com/mp/SimpleTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.jws.soap.SOAPBinding;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -22,5 +24,16 @@ public class SimpleTest {
         List<User> users = userMapper.selectList(null);
         Assert.assertEquals(5,users.size());
         users.forEach(System.out::println);
+    }
+
+    @Test
+    public void insert(){
+        User user = new User();
+        user.setName("刘明强");
+        user.setAge(31);
+        user.setManagerId(1088248166370832385L);
+        user.setCreateTime(LocalDateTime.now());
+        int rows = userMapper.insert(user);
+        System.out.println("影响记录数：" + rows);
     }
 }
