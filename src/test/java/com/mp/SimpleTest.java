8811dd038ac91d48797edebea80ea5c750b433c7
freesaber <saber.lvoer@qq.com>
2022-08-29 16:57:39 +0800

    1

--- src/test/java/com/mp/SimpleTest.java
+++ src/test/java/com/mp/SimpleTest.java
@@ -23,6 +23,8 @@ public class SimpleTest {
     @Autowired
     private UserMapper userMapper;
 
+    private QueryWrapper<User> queryWrapper;
+
     @Test
     public void selectById(){
         User user = userMapper.selectById(1094590409767661570L);
@@ -45,16 +47,44 @@ public class SimpleTest {
         users.forEach(System.out::println);
     }
 
+    private void printQueryWrapper(){
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
     /*
         名字中包含雨并且年龄小于40
      */
     @Test
     public void selectByWrapper(){
-        QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
-        //QueryWrapper<User> queryWrapper = Wrappers.<User>query();
-        queryWrapper.like("name", "雨");
-        
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "雨")
+                .lt("age", 40);
+        printQueryWrapper();
+    }
 
+    /*
+       名字中包含雨，并且年龄大于等于20，且小于等于40，并且email不为空
+    */
+    @Test
+    public void selectByWrapper2(){
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "雨")
+                .between("age", 20, 40)
+                .isNotNull("email");
+        printQueryWrapper();
+    }
 
+    /*
+       名字为王姓或者年龄大于等于25，按照年龄降序排列。年龄相同按照id升序排列
+    */
+    @Test
+    public void selectByWrapper3() {
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.likeRight("name","王")
+                .or()
+                .ge("age",25)
+                .orderByDesc("age").orderByAsc("id");
+        printQueryWrapper();
     }
 }
